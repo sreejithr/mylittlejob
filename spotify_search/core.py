a8fd8fc2e01f8f8893a37916f591598977eeec5e
Sreejith <sreejith@driveu.in>
2017-04-29 19:22:05 +0530

    Fix bugs in extracting thumbnails from search results

--- spotify_search/core.py
+++ spotify_search/core.py
@@ -38,21 +38,33 @@ class SpotifySearchAPI(BaseSpotifySearchAPI):
         w = keyword.replace('"', '')
         return w.replace(' ', '+')
 
-    def _add_thumbnails(self, results):
+    def _add_thumbnails(self, results, result_type):
         def _add_thumbnail(result):
             """
             Adds a `thumbnail` key. Since the image dimensions in the
             result aren't uniform, we select one which is atleast 150 x 150
             and closest in dimensions to it.
             """
+            if result_type == 'artist':
+                images = result['images']
+
+            elif result_type == 'album':
+                images = result['images']
+
+            elif result_type == 'playlist':
+                images = result['images']
+
+            elif result_type == 'track':
+                images = result['album']['images']
+
             try:
-                thumbnail = result['images'][0]
+                thumbnail = images[0]
             except IndexError:
                 # No images in the result. We let the frontend code handle this.
                 result['thumbnail'] = None
                 return result
 
-            for image in result['images']:
+            for image in images:
                 if image['width'] < 150 or image['height'] < 150:
                     continue
 
@@ -103,12 +115,24 @@ class SpotifySearchAPI(BaseSpotifySearchAPI):
 
         if r.status_code == 200:
             if self.filters.get('type', None) == 'artist':
-                return self._add_thumbnails(r.json().get('artists', None))
+                return self._add_thumbnails(
+                    r.json().get('artists', None),
+                    self.filters.get('type', None)
+                )
             elif self.filters.get('type', None) == 'album':
-                return self._add_thumbnails(r.json().get('album', None))
+                return self._add_thumbnails(
+                    r.json().get('albums', None),
+                    self.filters.get('type', None)
+                )
             elif self.filters.get('type', None) == 'playlist':
-                return self._add_thumbnails(r.json().get('playlist', None))
+                return self._add_thumbnails(
+                    r.json().get('playlists', None),
+                    self.filters.get('type', None)
+                )
             elif self.filters.get('type', None) == 'track':
-                return self._add_thumbnails(r.json().get('track', None))
+                return self._add_thumbnails(
+                    r.json().get('tracks', None),
+                    self.filters.get('type', None)
+                )
 
         return []

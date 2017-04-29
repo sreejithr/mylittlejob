3ccf2fb56ab0852ef3c286a202180627d77072b9
Sreejith <sreejith@driveu.in>
2017-04-29 16:17:10 +0530

    Replace Search class view with search view which accepts both GET and POST

--- spotify_search/views.py
+++ spotify_search/views.py
@@ -2,27 +2,79 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.generic import View
+from django.template.loader import render_to_string
 
 from .core import SpotifySearchAPI
 
 
 def home(request):
-    return render(request, 'spotify_search/index.html')
+    return render(request, 'spotify_search/index.html', context={})
 
 
-class Search(View):
+def search(request, *args, **kwargs):
     spotify = SpotifySearchAPI()
 
-    def get(self, request, *args, **kwargs):
-        keyword = request.GET.get('q', None)
+    if (request.method == 'POST'):
+        keyword = request.POST.get('q', None)
+        filter_by = request.POST.get('filter_by', 'artist')
+
+        if keyword:
+            result = spotify.search(keyword).filter_by(filter_by).execute()
+
+            if not result:
+                return JsonResponse({})
+
+            resultList = render_to_string(
+                'spotify_search/results.html', {
+                    'items': result.get('items', []),
+                    'type': filter_by,
+                }
+            )
+
+            data = {
+                'resultList': resultList,
+                'type': filter_by,
+                'count': result.get('total', 0),
+            }
+        else:
+            data = {
+                'resultList': [],
+                'type': filter_by,
+                'count': 0,
+            }
+
+        return JsonResponse(data)
+
+    else:
+        keyword = kwargs.get('keyword', None)
         filter_by = request.GET.get('filter_by', 'artist')
 
         if keyword:
-         result = {
-            'items': self.spotify.bsearch(keyword).filter_by(filter_by).execute(),
-            'type': filter_by
-        }
+            result = spotify.search(keyword).filter_by(filter_by).execute()
 
-        return render(request, "spotify_search/index.html")
+            if not result:
+                return render(request, "spotify_search/index.html", context={
+                    'items': [],
+                    'type': filter_by,
+                    'count': 0,
+                    'q': keyword,
+                })
+
+            context = {
+                'items': result.get('items', []),
+                'type': filter_by,
+                'count': result.get('total', 0),
+                'q': keyword,
+            }
+        else:
+            context = {
+                'items': [],
+                'type': filter_by,
+                'count': 0,
+                'error_msg': 'Please supply the search term',
+            }
+
+        return render(request, "spotify_search/index.html", context=context)
+
